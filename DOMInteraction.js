55c771b052b4c72b8eb0bb2b78fbdf40c156b6d2
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-07-04 10:56:30 -0500

    in-class JQ edits

--- DOMInteraction.js
+++ DOMInteraction.js
@@ -1,11 +1,11 @@
 {
 	let addMessages = Object.create(null);
-	let output = document.getElementById("msg-output");
+	let $output = $("#msg-output");
 	let combinedArray = [];
 
 	// adds new user message to private array
 	addMessages.addToArray = function() {
-		let userNewMsg = inputBox.value;
+		let userNewMsg = $inputBox.val();
 		combinedArray.unshift(userNewMsg);
 	};
 
@@ -16,11 +16,12 @@
 
 	// adds message to output box based on passed in ID and Message
   	addMessages.putMsgsInDom = function (id, msg) {
-  		let messageBlock = document.createElement('div');
-		messageBlock.setAttribute("id", `${id}`);
-		messageBlock.setAttribute("class", "message-container")
-		messageBlock.innerHTML = `<p class="msg">${msg}</p><button class="delete-btn">Delete</button>`;
-		output.appendChild(messageBlock);
+  		let $messageBlock = $("<div>", {id: `${id}`, class: "message-container"})
+  		// let messageBlock = document.createElement('div');
+		// messageBlock.setAttribute("id", `${id}`);
+		// messageBlock.setAttribute("class", "message-container")
+		$messageBlock.html (`<p class="msg">${msg}</p><button class="delete-btn">Delete</button>`);
+		$output.append($messageBlock);
 	}
 
 	// takes an array and pulls out the index and corresponding message, sends to putMsgsInDom
@@ -35,16 +36,22 @@
 	}
 
 	// functionality to change to dark theme or enlarged text
-	let increaseFontSize = document.querySelector(".biggerTextOption")
-	let addTheme = document.querySelector(".theme");
-
-	document.getElementById("dark-theme-box").addEventListener("click", function() {
-		addTheme.classList.toggle("dark");
+	$("#large-text-box").click(function(){ 
+		$(".biggerTextOption").toggleClass("large");
 	});
 
-	document.getElementById("large-text-box").addEventListener("click", function() {
-		increaseFontSize.classList.toggle("large");
-	});
+	$("#dark-theme-box").click(function() {
+		$(".theme").toggleClass("dark");
+	})
+	// let addTheme = document.querySelector(".theme");
+
+	// document.getElementById("dark-theme-box").addEventListener("click", function() {
+	// 	addTheme.classList.toggle("dark");
+	// });
+
+	// document.getElementById("large-text-box").addEventListener("click", function() {
+	// 	increaseFontSize.classList.toggle("large");
+	// });
 
 	window.Chatty = window.Chatty || {};
 	Chatty.DOMInteraction = addMessages;
--- JSON-fetcher.js
+++ JSON-fetcher.js
@@ -3,20 +3,15 @@
 	let messages = [];
 
 // retrieves messages from JSON file
-	loadMessages.retrieveMessages = function (assignIdFunc, jsonToArrayFunc) {
-		let requestMess = new XMLHttpRequest();
-		requestMess.open("GET", "preload.json");
-		requestMess.send();
-		requestMess.addEventListener("load", function () {
-			setMessages(assignIdFunc);
-			jsonToArrayFunc(messages);
+	loadMessages.retrieveMessages = function (assignIdFunc, jsonArrayFunc) {
+		$.ajax({
+			url: "preload.json"
+		})
+		.done(function(data) {
+			arrayUpMessages(data.preloaded_messages);
+			assignIdFunc(messages);
+			jsonArrayFunc(messages);			
 		});
-	} //
-// parses JSON messages into an array of objects, each with one key-value pair
-	function setMessages (cb) {
-		messagesObjs = JSON.parse(event.target.responseText).preloaded_messages;
-		arrayUpMessages(messagesObjs);
-		cb(messages);
 	}
 
  // takes array of objects and returns an array of strings, the 'messages'
